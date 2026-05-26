35fdc7e96d7361689ebc3ce4ec9bcc9aed045e27
marco-olivier <marcooliviert@gmail.com>
2026-05-26 14:00:00 +0200

    feat: add package init with core imports

---
+++ inferno/__init__.py
@@ -0,0 +1,6 @@
+"""Inferno Engine - High-performance AI/ML inference"""
+__version__ = "0.1.0"
+from .engine import Engine
+from .model import Model
+from .request import Request, Response
+from .session import Session
